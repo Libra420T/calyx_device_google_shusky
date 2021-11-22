7eb806c6aa6556d8a072e9f0f3f7065af3181679
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-22 21:22:09 +0530

    Add initial Calyx product makefiles
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: I0781b6d4d74680ce7f0d860048871da7c2d43fb6

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -23,7 +23,9 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/factory_husky.mk \
     $(LOCAL_DIR)/aosp_shiba.mk \
     $(LOCAL_DIR)/aosp_shiba_fullmte.mk \
-    $(LOCAL_DIR)/factory_shiba.mk
+    $(LOCAL_DIR)/factory_shiba.mk \
+    $(LOCAL_DIR)/calyx_husky.mk \
+    $(LOCAL_DIR)/calyx_shiba.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_ripcurrent-userdebug \
---
+++ husky/BoardConfig-calyx.mk
@@ -0,0 +1,1 @@
+include device/google/shusky/BoardConfig-calyx.mk
