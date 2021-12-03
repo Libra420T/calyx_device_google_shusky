de099d9ddaf8d99a6343496745418b5ae69dcc63
Chirayu Desai <chirayudesai1@gmail.com>
2021-12-04 01:01:42 +0530

    pantah: Set up proprietary files extraction
    * The proprietary files lists are generated by comparing stock partition
      contents with AOSP generated partitions.
    
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Change-Id: I17b78ccf212d8364070c40fe3c7d06d4048e3fe4

--- husky/BoardConfig-calyx.mk
+++ husky/BoardConfig-calyx.mk
@@ -1,1 +1,3 @@
 include device/google/shusky/BoardConfig-calyx.mk
+
+include vendor/google/husky/BoardConfigVendor.mk
--- shiba/BoardConfig-calyx.mk
+++ shiba/BoardConfig-calyx.mk
@@ -1,1 +1,3 @@
 include device/google/shusky/BoardConfig-calyx.mk
+
+include vendor/google/shiba/BoardConfigVendor.mk
