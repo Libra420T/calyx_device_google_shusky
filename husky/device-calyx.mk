6c3172c5bb5844e68764275aa5640919672f9467
Michael Bestas <mkbestas@lineageos.org>
2022-12-22 01:02:18 +0200

    shusky: overlay: Configure aux cameras for Aperture
    Change-Id: I3d1c93057b5c2a1ba09c06a945d538d2043f30fd

--- husky/device-calyx.mk
+++ husky/device-calyx.mk
@@ -1,3 +1,5 @@
+DEVICE_PACKAGE_OVERLAYS += device/google/shusky/husky/overlay-calyx
+
 TARGET_VENDOR_PROP += device/google/shusky/husky/vendor.prop
 
 $(call inherit-product, device/google/shusky/device-calyx.mk)
