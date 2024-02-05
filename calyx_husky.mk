b85de11de700116e3c9fc87385d1d62e38e1c8ec
Michael Bestas <mikeioannina@gmail.com>
2024-02-06 00:53:49 +0200

    Update fingerprint/build description from UQ1A.240205.004
    Change-Id: I883cac5968e85afbcf11a49d11514abafaa901d9

--- calyx_husky.mk
+++ calyx_husky.mk
@@ -13,9 +13,9 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=husky \
-    PRIVATE_BUILD_DESC="husky-user 14 UQ1A.240105.004 11206848 release-keys"
+    PRIVATE_BUILD_DESC="husky-user 14 UQ1A.240205.004 11269751 release-keys"
 
-BUILD_FINGERPRINT := google/husky/husky:14/UQ1A.240105.004/11206848:user/release-keys
+BUILD_FINGERPRINT := google/husky/husky:14/UQ1A.240205.004/11269751:user/release-keys
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
